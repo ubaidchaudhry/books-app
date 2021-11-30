3884a8f90dffb5e0afe9e6f42d7770bcbe12a8e0
ubaidchaudhry <ubaidchaudhry100k@gmail.com>
2021-11-30 18:36:21 +0500

    Adding filter to the books page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,19 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @tags = Tag.all
+    handle_search
     respond_to do |format|
       format.html { @books }
-      format.json {
-        render json: { books: @books }
-      }
+      format.json { render json: { books: @books } }
     end
   end
+
+    def destroy
+      @books = Book.find(params[:id])
+    @books.destroy
+    redirect_to action: "index"
+    end
 
   def assign_tag
     @tags = Tag.all
@@ -21,6 +27,17 @@ class BooksController < ApplicationController
       BookTag.find_or_create_by(book_id: book.id, tag_id: tag.id)
     end
     redirect_to books_path
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_books
+    @books = Book.find(params[:books_ids])
+  end
+
+  def handle_search
+    @books = @books.joins(:tags).where("tags.id = ?", params["tag_id"]) if params["tag_id"].present?
+    @books = @books.where(price: params["search_price"].to_i) if params["search_price"].present?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index] do
+  resources :books, only: [:index, :destroy] do
     collection do
       get :assign_tag
       put :assign_tag_to
